8838875df0ebce58108dd5301b946b414464b573
Marino Carranza <marino.carranza@gmail.com>
2019-07-22 16:43:16 -0700

    adds images needed, stops timer and adds listener to the possible answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,14 +6,19 @@ var questionNumber=0;
 var choiceA=$('<button>');
 var choiceB=$('<button>');
 var choiceC=$('<button>');
-var choiceD=$('<button>');
+var choiceD = $('<button>');
+var firstChoice;
+var secondChoice;
+var thirdChoice;
+var fourthChoice;
 var correct=0;
 var wrong=0;
 var unanswered=0;
 var playing = false;
-var seconds = 20;
-var intervalId;
-    
+var seconds = 5;
+var intervalId=0;
+var choiceMade;
+var algoAqui;  
 var questions = [
     {
         'question': 'How many different species of birds can be found in Guatemala?',
@@ -63,65 +68,69 @@ var questions = [
 
 
     // adding button to page
-    function startButton() {
+function startButton() {
 
-        var $heading = $('#titulo');
-        $heading.addClass('top-text');
-        $heading.html('<h1>Trivia Game !</h1>');
+    var $gameStart = $('#top');
+    $gameStart.addClass('start-boton');
 
-        var $gameStart = $('#top');
-        $gameStart.addClass('start-boton');
-
-        var boton = $('<button>');
-        boton.addClass('press').text('Start !');
-        $gameStart.append(boton);
-    }
-    startButton();
+    var boton = $('<button>');
+    boton.addClass('press').text('Start !');
+    $gameStart.append(boton);
+}
+startButton();
 
     // Games starts on when start button is clicked and after click button dissappears from screen the timer appears along with the first question
 function start() {
     $('button').on('click', function () {
         $('.press').hide();
 
-        // adding the timer to the page starting at 20 secs
-        function run() {
-            clearInterval(intervalId);
-            intervalId = setInterval(countDown, 1000);
-        }
-        //  The countDown function.
-        function countDown() {
-            //  Decrease number by one.
-            seconds--;
-            var redSeconds = "<span style='color:red;font-size:200%;'>" + seconds + "</span>"
-            //  adding the numbers to the #timer div.
-            $('#timer').html('<p> You have :  ' + redSeconds + '  seconds to answer </p>');
-        }
-        run();
+// adding the timer to the page starting at 20 secs
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(countDown, 1000);
+//  The countDown function.
+function countDown() {
+//  Decrease number by one.
+    seconds--;
+    var redSeconds = "<span style='color:red;font-size:200%;'>" + seconds + "</span>"
+    //  adding the numbers to the #timer div.
+$   ('#timer').html('<p> You have :  ' + redSeconds + '  seconds to answer </p>');
 
-        // adding the question and the list of possible answers
-
-        var questionHere = $("<h3>").text(questions[questionNumber].question);
-        questionHere.addClass('pregunta');
-        $('#question').append(questionHere); 
-        
-        // list of choice possible answers
-
-        var firstChoice = $('<button>').text(questions[questionNumber].choiceA);
-        firstChoice.addClass('choice').attr('data-click');
-        var secondChoice = $('<button>').text(questions[questionNumber].choiceB);
-        secondChoice.addClass('choice').attr('data-click');
-        var thirdChoice = $('<button>').text(questions[questionNumber].choiceC);
-        thirdChoice.addClass('choice').attr('data-click');
-        var fourthChoice = $('<button>').text(questions[questionNumber].choiceD);
-        fourthChoice.addClass('choice').attr('data-click');
-        $('#choices').append(firstChoice).append(secondChoice).append(thirdChoice).append(fourthChoice);
-        questionNumber++;
-
-        // checking to see if the answer is right or not
-
-        
-
-    })
+    // if timer reaches 0 move to unanswered window eventually
+    if (seconds === 0) {
+    unanswered++;
+        console.log('TIMER REACHED 0');
+        clearInterval(intervalId);
+    }
 }
 
-    start();
+}// closes run function
+        
+run();
+
+        // adding the question and the list of possible answers
+        var questionHere = $("<h3>").text(questions[questionNumber].question);
+        questionHere.addClass('pregunta');
+        $('#question').append(questionHere);
+        
+        // list of choice possible answers
+        firstChoice = $('<button>').text(questions[questionNumber].choiceA);
+        firstChoice.addClass('choice').attr('data-click');
+        secondChoice = $('<button>').text(questions[questionNumber].choiceB);
+        secondChoice.addClass('choice').attr('data-click');
+        thirdChoice = $('<button>').text(questions[questionNumber].choiceC);
+        thirdChoice.addClass('choice').attr('data-click');
+        fourthChoice = $('<button>').text(questions[questionNumber].choiceD);
+        fourthChoice.addClass('choice').attr('data-click');
+        $('#choices').append(firstChoice).append(secondChoice).append(thirdChoice).append(fourthChoice);
+    })
+
+    if ($('#choices').on('click', function () {
+            // $('.choice').hide();
+        console.log('choice got clicked');
+        $('.choice').hide();
+    }));
+    questionNumber++;
+}  // this is the closing bracket for the start function
+start();
+    
